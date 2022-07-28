38dba312deab39685ad9deca91decde2b6f2be77
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-07-28 10:56:30 +0300

    added useState hook in Catalogue.js

--- catalogue/src/App.js
+++ catalogue/src/App.js
@@ -1,31 +1,11 @@
-import React, { useState, useEffect } from "react";
-import ProductsContainer from "./components/ProductsContainer"
+
 
 
 function App() {
 
 
 
-  function updateProducts(product) {
-    setProducts([...products, product]);
-  }
-
-const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/products")
-      .then((response) => response.json())
-      .then((products) => setProducts(products));
-  }, []);
-
- 
-
-      function addProducts(product){
-        setProducts([...products,product])
-      }
-
-
-
+  
   return (
     <div className="app">
         
