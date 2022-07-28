2517f05e8774f4217d8aa52309973a33be23586c
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-07-28 11:18:14 +0300

    installed react-router-dom

--- catalogue/src/App.js
+++ catalogue/src/App.js
@@ -1,4 +1,5 @@
 import {  Routes, Route } from "react-router-dom";
+import React from "react";
 import Navbar from "./components/Navbar";
 import Home from './components/Home';
 import Catalogue from './components/Catalogue';
