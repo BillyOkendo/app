13bbdbf39072afbc4b1aa4a71b7367e140b3dcdb
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-07-31 17:30:12 +0300

    updated reviews page

--- catalogue/src/components/Reviews.js
+++ catalogue/src/components/Reviews.js
@@ -5,21 +5,25 @@ function Reviews() {
     <div className='reviews'>
 <h1>CUSTOMER REVIEWS</h1>
 
-<h1>Wanjiru</h1>
-<p>Good products</p>
+<h1>Haniel</h1>
+<p>The quality of the product I purchased was satisfactory.<br/>
+   Looking to buy from here again!</p>
 
 <h1>Gabby</h1>
-<p>Best products</p>
+<p>The delivery time can be much better. <br/>
+  All in all, impressed by the service.</p>
 
-<h1>Billy</h1>
-<p>Amazing life</p>
+<h1>Stacy</h1>
+<p>I could buy the whole catalogue if I had the money.<br/>
+   Good job guys!</p>
 
 
-<h1>Vera</h1>
-<p>Best products</p>
+<h1>Monique</h1>
+<p>Easy site to use when purchasing stuff online. <br/>
+  I loved the mug that I bought.</p>
 
 <h1>Stephen</h1>
-<p>Best products</p>
+<p>Cool service.</p>
 
 
     </div>
